61feaa891d0d87242b9ac54c2ac3aa79002318a2
anasAbdullfattah <anasAbdullfattah@gmail.com>
2025-11-09 14:05:35 +0200

    Initialize project with Express, Mongoose, dotenv, and nodemon
    - Set up basic server structure
    - Installed and configured dependencies

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is Ready");
+});
+app.listen(PORT, () => {
+  console.log("the server is running");
+});
